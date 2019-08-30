cd17ec2f43e7c96a10604ad2e3e2f22b98f77e9c
Imaraika <inange2013@gmail.com>
2019-08-30 20:12:43 +0200

    changes haves been added

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { RouterModule } from '@angular/router';
+import { RoutingModule } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    RepositoriesComponent,
+    SearchFormComponent,
+    UserInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    RoutingModule,
+    RouterModule,
+    FormsModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
