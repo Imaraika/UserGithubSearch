bbf54d4956d901c59814138834d327ae948ba8b2
Imaraika <inange2013@gmail.com>
2019-09-02 09:01:52 +0200

    changes have been added

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -4,6 +4,7 @@ import {Search} from '../search';
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
+  
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import {SearchRequestService} from '../search-request.service';
+import {Repository} from '../repository';
+import {User} from '../user';
 
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
+  providers: [SearchRequestService],
   styleUrls: ['./user-info.component.css']
 })
 export class UserInfoComponent implements OnInit {
 
-  constructor() { }
+  public searchMe = 'Imaraika';
+  public githubUser: string;
 
-  ngOnInit() {
+  users: User ;
+  repository: Repository;
+  public searchRepo: string;
+  public resultCount = 12;
+
+
+  findUser(username) {
+      this.githubUser = '';
+      this.searchMe  = username;
+      this.ngOnInit();
   }
 
+
+constructor(public githubUserRequest: SearchRequestService, public userRepos: SearchRequestService) { }
+
+ngOnInit() {
+    this.githubUserRequest.githubUser(this.searchMe);
+    this.users = this.githubUserRequest.users;
+    this.userRepos.gitUserRepos(this.searchMe);
+    console.log(this.userRepos);
+}
+
+
+  searchRepos() {
+      this.searchRepo = '';
+      this.resultCount = 10;
+
+  }
 }
